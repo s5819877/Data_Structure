f43e31a5e7df7df051dd4a1913639e985964a9f7
s5819877 <5819877@stu.kmu.ac.kr>
2026-05-10 22:01:17 +0900

    Add files via upload

---
+++ Data_Structure/Assignment/A12/LinkedList.c
@@ -0,0 +1,97 @@
+#include "LinkedList.h"
+
+LinkedList* createLinkedList() {
+	struct linkedList* r;
+	r = (struct linkedList*)malloc(sizeof(struct linkedList));
+
+	r->head = (struct pointType*)NULL;
+	r->size = 0;
+
+	return r;
+}
+
+int destroyLinkedList(LinkedList* li) {
+	struct pointType* nptr, * mptr;
+	for (nptr = li->head; nptr != NULL; nptr = mptr) {
+		mptr = nptr->next;
+		free(nptr);
+	}
+
+	free(li);
+
+	return 1;
+}
+
+int isEmptyLinkedList(LinkedList* li) {
+	return (li->size == 0);
+}
+
+int sizeLinkedList(LinkedList* li) {
+	return li->size;
+}
+
+LinkedList* insertFirstLinkedList(LinkedList* li, PointType item) {
+	PointType* ptr = (PointType*)malloc(sizeof(PointType));
+	*ptr = item;
+
+	ptr->next = li->head;
+	li->head = ptr;
+	li->size++;
+
+	return li;
+}
+
+int printLinkedList(LinkedList* li) {
+	PointType* current = li->head;
+	printf("LinkedList:\n");
+
+	for (int i = 0; i < li->size; i++) {
+		printf("[%d] x:%d, y:%d, next:%x\n",
+			i, current->x, current->y, current->next);
+
+		current = current->next;
+	}
+}
+
+LinkedList* insertLastLinkedList(LinkedList* li, PointType item) {
+	PointType* ptr = li->head;
+
+	while (ptr->next != NULL) {
+		ptr = ptr->next;
+	}
+
+	PointType* nPtr = (PointType*)malloc(sizeof(PointType));
+	*nPtr = item;
+	nPtr->next = NULL;
+
+	ptr->next = nPtr;
+
+	li->size++;
+
+	return li;
+}
+
+LinkedList* deleteAtLinkedList(LinkedList* li, int at) {
+	if (at >= 0 && at < li->size) {
+		PointType* nPtr = li->head;
+
+		for (int i = 0; i < at - 1; i++) { // 기존 at까지 돌던 루프 at-1로 수정
+			nPtr = nPtr->next;
+		}
+
+		if (at == 0) {
+			PointType* fPtr = li->head;
+			li->head = fPtr->next;
+			free(fPtr);
+		}
+		else {
+			PointType* fPtr = nPtr->next;
+			nPtr->next = fPtr->next;
+			free(fPtr);
+		}
+
+		li->size--; // 삭제 후 사이즈 감소 추가
+	}
+
+	return li; // 반환형에 맞게 추가
+}
