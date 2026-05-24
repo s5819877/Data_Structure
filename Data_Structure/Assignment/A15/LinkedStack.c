7f2b50f6cb2e85e029d5459ebcdf9d9bf02ae181
s5819877 <5819877@stu.kmu.ac.kr>
2026-05-24 18:19:48 +0900

    Add files via upload

---
+++ Data_Structure/Assignment/A15/LinkedStack.c
@@ -0,0 +1,71 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "LinkedStack.h"
+
+LinkedStack* createLinkedStack() {
+    LinkedStack* re = (LinkedStack*)malloc(sizeof(LinkedStack));
+    re->head = NULL;
+    re->size = 0;
+
+    return re;
+}
+
+int destroyLinkedStack(LinkedStack* s) {
+    Node* temp = s->head;
+    while (temp != NULL) {
+        Node* next = temp->next;
+        free(temp->data);
+        free(temp);
+        temp = next;
+    }
+    free(s);
+
+    return 1;
+}
+
+int emptyLinkedStack(LinkedStack* s) {
+    return s->size == 0;
+}
+
+int fullLinkedStack(LinkedStack* s) {
+    return 0;
+}
+
+int pushLinkedStack(LinkedStack* s, stackElement item) {
+    Node* temp = (Node*)malloc(sizeof(Node));
+    temp->data = (char*)malloc(sizeof(char) * strlen(item) + 1);
+    strcpy(temp->data, item);
+    temp->next = s->head;
+    s->head = temp;
+    s->size++;
+    return 1;
+}
+
+stackElement popLinkedStack(LinkedStack* s) {
+    if (emptyLinkedStack(s)) {
+        return (stackElement)NULL;
+    }
+
+    Node* temp = s->head;
+    stackElement data = temp->data;
+    s->head = temp->next;
+    free(temp);
+    s->size--;
+    return data;
+}
+
+stackElement peekLinkedStack(LinkedStack* s) {
+    if (emptyLinkedStack(s)) {
+        return (stackElement)NULL;
+    }
+    return s->head->data;
+}
+
+void printLinkedStack(LinkedStack* s) {
+    printf("Stack:\n");
+    printf("Size: %d\n", s->size);
+    Node* temp = s->head;
+    while (temp != NULL) {
+        printf("%s\n", temp->data);
+        temp = temp->next;
+    }
+}
---
+++ Data_Structure/Assignment/A15/main.c
@@ -0,0 +1,65 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "LinkedStack.h"
+
+void printMenu() {
+    printf("======= 스택 메뉴 =======\n");
+    printf("1. URL 추가\n");
+    printf("2. URL 제거\n");
+    printf("3. 스택 확인\n");
+    printf("=========================\n");
+    printf("선택 : ");
+}
+
+int main() {
+    LinkedStack* myStack = createLinkedStack();
+    int choice;
+    char url[256];
+
+    while (1) {
+        printMenu();
+        scanf("%d", &choice);
+
+        switch (choice) {
+        case 1:
+            printf("URL 입력 : ");
+            scanf("%s", url);
+            pushLinkedStack(myStack, url);
+            printf("현재 창 : %s\n\n", peekLinkedStack(myStack));
+            break;
+
+        case 2:
+            if (emptyLinkedStack(myStack)) {
+                printf("스택이 비어있습니다.\n\n");
+                break;
+            }
+
+            stackElement popstack = popLinkedStack(myStack);
+            printf("이탈 : %s\n", popstack);
+            free(popstack);
+
+            if (emptyLinkedStack(myStack)) {
+                printf("브라우저가 종료되었습니다.\n");
+                destroyLinkedStack(myStack);
+                return 0;
+            }
+            else {
+                printf("현재 창 : %s\n\n", peekLinkedStack(myStack));
+            }
+
+            break;
+
+        case 3:
+            if (emptyLinkedStack(myStack)) {
+                printf("스택이 비어있습니다.\n\n");
+            }
+            else {
+                printf("현재 창 : %s\n\n", peekLinkedStack(myStack));
+            }
+            break;
+
+        default:
+            printf("잘못된 입력입니다. 다시 입력해주세요\n\n");
+            break;
+        }
+    }
+}
